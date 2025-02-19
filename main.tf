d4551796622542532f1cff1429198eb90d862a46
vrapracash <veeraprakash.bg@gmail.com>
2025-02-19 10:32:14 +0530

    init-update

--- main.tf
+++ main.tf
@@ -1,21 +1,20 @@
 resource "aws_security_group" "main" {
-  name        = "local.sg_final_name"
-  description = "Allow TLS inbound traffic"
+  name        = local.sg_final_name
+  description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = var.vpc_id
 
-  
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  tags = merge (
+  tags = merge(
     var.common_tags,
     var.sg_tags,
     {
-        Name = "local.sg_final_name"
+        Name = local.sg_final_name
     }
   )
 }
